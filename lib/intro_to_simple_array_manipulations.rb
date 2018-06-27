a0710803308f36aa7a2422f146fb8e3a5f99da95
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-06-27 16:15:26 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
 def using_push(array, element)
-  
+  array.push(element)
 end
 
 def using_unshift(array, element)
@@ -7,7 +7,43 @@ def using_unshift(array, element)
 end
 
 def using_pop(array)
+  array.pop()
+end
+
+def pop_with_args(number_of_elements)
+  array.pop(number_of_elements)
+end
+
+def using_unshift
   
 end
 
-def pop_with_args()
+def shift_with_args
+  
+end
+
+def using_concast
+  
+end
+
+def using_insert
+  
+end
+
+def using_uniq
+  
+end
+
+def using_flatten
+  
+end
+
+def using_delete
+  
+end
+
+def using_delete_at
+  
+end
+
+
