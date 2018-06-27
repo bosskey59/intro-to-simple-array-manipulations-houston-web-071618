99865f4ea7157c3296dfb0387d811f1fbd7c5be9
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-06-27 16:18:05 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,16 +10,16 @@ def using_pop(array)
   array.pop()
 end
 
-def pop_with_args(number_of_elements)
-  array.pop(number_of_elements)
+def pop_with_args(array)
+  array.pop(2)
 end
 
 def using_shift(array)
   array.shift()  
 end
 
-def shift_with_args
-  
+def shift_with_args(array)
+  array.shift(2)
 end
 
 def using_concast
